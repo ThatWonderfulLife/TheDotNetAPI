4ea54eb534d1b072163838fcb45e3d52b029dd55
Wonder <Wonder@DESKTOP-OQHLTUN>
2026-03-17 19:40:02 -0300

    Sync with devices

--- WebApplication1/Controllers/CityController.cs
+++ WebApplication1/Controllers/CityController.cs
@@ -1,83 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using PostgresAPI.Models;
 using System.Net;
 
 namespace WebApplication1.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class CityController : ControllerBase
+    public static class CityController
     {
-        private readonly AppDbContext _context;
-
-        public CityController(AppDbContext context)
+        static async Task<Results<Ok<City>, BadRequest>> GetCityById([FromServices] AppDbContext context, int id)
         {
-            _context = context;
-        }
+            if (id <= 0)
+            {
+                return (TypedResults.BadRequest());
+            }
+            var city = await context.City.FindAsync(id);
 
-        [HttpGet("GetCities")]
-        public async Task<ActionResult<City[]>> GetCities()
+            if (city == null)
+            {
+                return TypedResults.BadRequest();
+            }
+
+            return TypedResults.Ok(city);
+        }   
+        public static async Task<Ok<List<City>>> GetAllCities([FromServices] AppDbContext context)
         {
-            var result = await _context.City.ToArrayAsync();
-
-            return Ok(result);
+            var cities = await context.City.ToListAsync();
+            return TypedResults.Ok(cities);
         }
-
-
-        [HttpGet("GetCityById")]
-        public async Task<IActionResult> GetCityById(int cityId)
-        {
-            var category = await _context.City.Where(x => x.Id == cityId).FirstOrDefaultAsync();
-
-            if (category == null)
-                return NotFound();
-
-            return Ok(category);
-        }
-
-        [HttpPost("CreateCategory")]
-        public async Task<IActionResult> CreateCategory([FromBody] Category category)
-        {
-            if (category == null)
-                return BadRequest("Invalid Request");
-
-            category.LastUpdate = DateTime.UtcNow;
-            _context.Category.Add(category);
-            await _context.SaveChangesAsync();
-
-
-            return CreatedAtAction(
-                nameof(CreateCategory),
-                new { id = category.Id },
-                category
-                );
-        }
-
-        [HttpPut("EditCategory")]
-        public async Task<IActionResult> EditCategory([FromBody] Category category)
-        {
-            var rows = await _context.Category.Where(x => x.Id == category.Id)
-                .ExecuteUpdateAsync(update => update
-                .SetProperty(x => x.Name, category.Name)
-                .SetProperty(x => x.LastUpdate, DateTime.UtcNow)
-                );
-            if (rows == 0)
-                return NotFound();
-
-            return Ok(category);
-        }
-
-        [HttpDelete("DeleteCategory")]
-        public async Task<IActionResult> DeleteCategory(int categoryId)
-        {
-            var rows = await _context.Category.Where(x => x.Id == categoryId).ExecuteDeleteAsync();
-
-            if (rows == 0)
-                return NotFound();
-
-            return Ok(true);
-        }
-
     }
+
 }
--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -4,6 +4,7 @@ using PostgresAPI.Models;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Scalar.AspNetCore;
+using WebApplication1.Controllers;
 var builder = WebApplication.CreateBuilder(args);
 
 
@@ -27,19 +28,10 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
+app.MapGet("/cities",CityController.GetAllCities);
+
 app.MapControllers();
 
-static async Task<Results<Ok<City>,BadRequest>> getCityById([FromServices] AppDbContext context, int id)
-{
-    if(id <= 0)
-    {
-        return (TypedResults.BadRequest());
-    }
-    var cities = await context.City.FindAsync(id);
-    return TypedResults.Ok(cities);
-}
-
-app.MapGet("/city/{id}",getCityById);
 
 app.Run();
 
